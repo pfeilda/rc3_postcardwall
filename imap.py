df8447b2f11c3590dcd06d77f4d626cee838cb65
Mike Tsenatek <10235033+MikeTsenatek@users.noreply.github.com>
2020-12-26 18:08:26 +0100

    Set flag 'SEEN' after Download

--- imap.py
+++ imap.py
@@ -2,15 +2,13 @@
 
 import configparser
 import ssl
-from imapclient import IMAPClient
+from imapclient import IMAPClient, SEEN
 import email
-from email.utils import parsedate_to_datetime, parsedate_tz, mktime_tz
+from email.utils import parsedate_tz, mktime_tz
 from PIL import Image
 import os
 import io
 import logging
-from datetime import datetime
-import time
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s: %(message)s',
@@ -26,31 +24,28 @@ ssl_context = ssl.create_default_context()
 def crop(path, input):
     im = Image.open(io.BytesIO(input))
     imgwidth, imgheight = im.size
-    upper = im.crop((0, 0, imgwidth, imgheight/2 - 1))
-    lower = im.crop((0, imgheight/2, imgwidth, imgheight))
+    upper = im.crop((0, 0, imgwidth, imgheight / 2 - 1))
+    lower = im.crop((0, imgheight / 2, imgwidth, imgheight))
     upper.save(os.path.join(path + '-upper.png'))
     lower.save(os.path.join(path + '-lower.png'))
 
 
 with IMAPClient(config['imap']['host'], ssl_context=ssl_context) as server:
     server.login(config['imap']['user'], config['imap']['passwd'])
-    server.select_folder('INBOX', readonly=True)
+    server.select_folder('INBOX')
 
     messages = server.search('UNSEEN')
     for uid, message_data in server.fetch(messages, 'RFC822').items():
         email_message = email.message_from_bytes(message_data[b'RFC822'])
-        print(mktime_tz(parsedate_tz(email_message.get('Date'))))
-        print(email_message.get('Message-ID')[1:][:-1].split("@")[0])
         for part in email_message.walk():
-            print(part.get_content_type())
             if part.get_content_type() == 'image/jpeg':
-                fileName = str(mktime_tz(parsedate_tz(email_message.get('Date'))))+'_'+email_message.get('Message-ID')[1:][:-1].split("@")[0]+'.jpg'
+                print(email_message.get('Message-ID'))
+                fileName = str(mktime_tz(parsedate_tz(email_message.get('Date')))) + '_' \
+                           + email_message.get('Message-ID').strip()[1:][:-1].split("@")[0] + '.jpg'
                 if bool(fileName):
                     filePath = os.path.join('./cards/', fileName)
                     if not os.path.isfile(filePath):
                         fp = open(filePath, 'wb')
                         fp.write(part.get_payload(decode=True))
                         fp.close()
-                crop('./cards/'+fileName, part.get_payload(decode=True))
-
-
+                crop('./cards/' + fileName, part.get_payload(decode=True))
